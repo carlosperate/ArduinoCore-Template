7ee3841e0ccce58d174371a98aed34b1d25d6b36
carlosperate <carlosperate@users.noreply.github.com>
2019-12-30 20:19:49 +0000

    Core: Update sourcecode to conform to coding style.

--- .github/actions/clang-format/entrypoint.sh
+++ .github/actions/clang-format/entrypoint.sh
@@ -8,7 +8,7 @@ if [ -z $1 ]; then
     python .github/actions/clang-format/run-clang-format.py \
         --recursive . \
         --extensions c,h,C,H,cpp,hpp,cc,hh,c++,h++,cxx,hxx,ino \
-        --color always
+        --color always \
         --style Google
 else
     # Pass arguments to run-clang-format (set -x prints cmds run)

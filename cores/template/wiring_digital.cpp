0a96a06e9ade60b6eac86f2a0a10c2237a2a1ff0
Carlos Pereira Atencio <carlosperate@embeddedlog.com>
2022-12-29 23:55:45 +0000

    core: Add wiring_digital.cpp with the digital pin functions.

---
+++ cores/template/wiring_digital.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file wiring_digital.cpp
+ * @brief Function definitions for setting pin mode, digital read and write.
+ */
+#include "api/Common.h"
+
+/**
+ * Set a pin to a specific mode (input or output).
+ *
+ * Input pull mode can also be enabled using this function.
+ *
+ * @param pinNumber The Arduino pin to configure.
+ * @param pinMode The mode to set the pin.
+ */
+void pinMode(pin_size_t pinNumber, PinMode pinMode) {
+    if (!isPinDigital(pinNumber)) {
+        // Throw error
+    }
+
+    switch (pinMode) {
+        case INPUT: break;
+        case OUTPUT: break;
+        case INPUT_PULLUP: break;
+        case INPUT_PULLDOWN: break;
+        case OUTPUT_OPENDRAIN: break;
+        default: break;
+    }
+}
+
+/**
+ * Set a pin to a digital HIGH or LOW state.
+ *
+ * If the pin is configured as an input, this function will instead set the
+ * pull up (HIGH) or down (LOW).
+ *
+ * @param pinNumber The Arduino pin number to write.
+ * @param status The state to set the pin, either `HIGH` or `LOW`.
+ */
+void digitalWrite(pin_size_t pinNumber, PinStatus status) {
+    if (!isPinDigital(pinNumber)) {
+        // Error
+    }
+}
+
+/**
+ * Read the digital state of a pin.
+ *
+ * @param pinNumber The Arduino pin number to read.
+ * @return The digital state of the pin, either `HIGH` or `LOW`.
+ */
+PinStatus digitalRead(pin_size_t pinNumber) {
+    if (!isPinDigital(pinNumber)) {
+        // Error
+    }
+}
